ba8c261e8ba18ebc0660451e04b61b349337a12d
dpascualhe <d.pascualhe@gmail.com>
2017-03-14 01:56:33 +0100

    Keeping everything tidy

--- digitclassifier.py
+++ digitclassifier.py
@@ -1,31 +1,31 @@
-'''
-Created on Mar 2, 2017
+#
+# Created on Mar 2, 2017
+#
+# @author: dpascualhe
+#
+# It receives images from a live video and classify them into digits 
+# employing a convolutional neural network written with Keras library.
+# It shows the live video and the results in a GUI.
+#
+# Based on @nuriaoyaga code:
+# https://github.com/RoboticsURJC-students/2016-tfg-nuria-oyaga/blob/
+#     master/numberclassifier.py
+#
 
-@author: dpascualhe
+import sys
+import signal
 
-It receives images from a live video and classify them into digits employing a
-convolutional neural network written with Keras library. It shows the live
-video and the results in a GUI.
+from PyQt5 import QtWidgets
 
-Based on @nuriaoyaga code:
-https://github.com/RoboticsURJC-students/2016-tfg-nuria-oyaga/blob/master/numberclassifier.py
-
-'''
-
-from Camera.cameraview import Cameraview
 from Camera.camera import Camera
 from Camera.threadcamera import ThreadCamera
 from GUI.gui import GUI
 from GUI.threadgui import ThreadGUI
-from PyQt5 import QtWidgets
-import sys
-import signal
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 if __name__ == '__main__':
-    
-    # We define the classes that we're going to need
+     
     cam = Camera()
     app = QtWidgets.QApplication(sys.argv)
     window = GUI()
@@ -41,3 +41,4 @@ if __name__ == '__main__':
     t_gui.start()
     
     sys.exit(app.exec_())
+
