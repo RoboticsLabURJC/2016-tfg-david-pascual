53d241b4ef25c66ebfa00330d771d8d4b5cddcc9
dpascualhe <d.pascualhe@gmail.com>
2017-06-08 15:55:00 +0200

    Added weight visualization

--- Net/layer_visualization.py
+++ Net/layer_visualization.py
@@ -7,63 +7,86 @@
 # and their weights.
 #
 
-import os
 import math
+import os
 
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 from keras import backend as K
-from keras.utils import vis_utils
 from keras.models import load_model, Model
+from keras.utils import vis_utils
 
-def plot_activations(nb_filters, activations):
-    ''' Plots the activations obtained between layers '''
-    for i in range(nb_filters):
-        plt.subplot(math.ceil(nb_filters/7.), 7, i+1)
-        plt.imshow(activations[0][:,:,i])
-    plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
 
-if __name__ == "__main__":
-    model_path = raw_input("Net path: ")
-    while not os.path.isfile(model_path):
-        model_path = raw_input("Enter a valid path: ")
-
-    im_path = raw_input("Image path: ")
-    while not os.path.isfile(im_path):
-        im_path = raw_input("Enter a valid path: ")
+class Layer_Visualization:
+    def __init__(self, model, im):
+        self.model = model        
+        # We adapt the image shape.
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        self.im = im.reshape(1, im.shape[0], im.shape[1], 1)
     
-    model = load_model(model_path)
-
-    # We read the image.
-    im = cv2.imread(im_path)
-    cv2.imshow("Sample", im)
-    cv2.waitKey(2000)
-    cv2.destroyAllWindows()
+    def norm(self, x):
+        y = (x - np.amin(x)) / (np.amax(x) - np.amin(x)) * 255
+        return y
     
-    # We adapt the image shape.
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    if K.image_dim_ordering() == "th":
-        im = im.reshape(1, 1, im.shape[0], im.shape[1])            
-    else:      
-        im = im.reshape(1, im.shape[0], im.shape[1], 1)
-    
-    # First convolutional network activation and its plot
-    first_conv = Model(input=model.inputs,
-                       output=model.get_layer("conv2d_1").output)
-    first_conv_activation = first_conv.predict(im)
-    plot_activations(first_conv_activation.shape[3], first_conv_activation)
-    first_conv_weights = model.get_layer("conv2d_1").get_weights()
-    print("1st conv. layer weights shape: ", first_conv_weights[0].shape)
-    vis_utils.plot_model(first_conv, "1st_conv.png", show_shapes=True)
-
-    # Second convolutional network activation and its plot
-    second_conv = Model(input=model.input,
-                        output=model.get_layer("conv2d_2").output)
-    second_conv_activation = second_conv.predict(im)
-    plot_activations(second_conv_activation.shape[3], second_conv_activation)
-    second_conv_weights = model.get_layer("conv2d_2").get_weights()
-    print("2nd conv. layer weights shape: ", second_conv_weights[0].shape)
-    vis_utils.plot_model(second_conv, "2nd_conv.png", show_shapes=True)
-    
-    
+    def plot_data(self, data_name, layer, nb_filters, data, i, j, depth,
+                  interp="none", color="jet"):
+        plt.figure(data_name + " - " + layer.get_config()["name"])
+        size = math.sqrt(nb_filters * depth)
+        plt.subplot(math.ceil(size), math.ceil(size), i + 1 + (j*depth))
+        plot = plt.imshow(data, interpolation=interp, vmin=0, vmax=255)
+        plot.set_cmap(color)
+        
+    def visualization(self, interp="none", color="jet"):
+        pred = np.argmax(self.model.predict(self.im))
+        print "\nDigit prediction: ", pred, "\n"
+        
+        for i, layer in enumerate(self.model.layers):
+            if layer.get_config()["name"][:6] == "conv2d":
+                # Getting weights
+                shape = layer.get_weights()[0].shape
+                weights = self.norm(layer.get_weights()[0].reshape(shape[2], shape[0],
+                                                              shape[1], shape[3]))
+                print "-------------", layer.get_config()["name"], "-------------"
+                print "Filters:"
+                print "    Width: ", weights.shape[1]
+                print "    Height: ", weights.shape[2]
+                print "    Depth: ", weights.shape[0]
+                print "    Number of filters: ", weights.shape[3], "\n"
+        
+                # Getting activations
+                truncated = Model(inputs=self.model.inputs, outputs=layer.output)
+                activations = self.norm(truncated.predict(self.im))
+                print "Activations:"
+                print "    Width: ", activations.shape[1]
+                print "    Height: ", activations.shape[2]
+                print "    Depth: ", activations.shape[0]
+                print "    Number of activation maps: ", activations.shape[3]
+        
+                nb_filters = weights.shape[3]
+                for j in range(nb_filters):
+                    # Weights
+                    filter_depth = 1 # weights.shape[0]
+                    for k in range(filter_depth):
+                        filter = weights[k][:, :, j]
+                        self.plot_data("Weights", layer, nb_filters, filter, k, j,
+                                  filter_depth, interp, color)
+            
+                        # Weights gradient
+                        sobelx = cv2.Sobel(filter, cv2.CV_32F, 1, 0, ksize=5)
+                        sobely = cv2.Sobel(filter, cv2.CV_32F, 0, 1, ksize=5)
+                        sobel = self.norm(abs(sobelx + sobely))
+                        self.plot_data("Weights gradient", layer, nb_filters, sobel, k,
+                                  j, filter_depth, interp, color)
+                        
+                    # Activation maps
+                    activation_map = activations[0][:, :, j]
+                    self.plot_data("Activation maps", layer, nb_filters, activation_map,
+                              0, j, 1, interp, color)
+                plt.show()
+                
+if __name__ == "__main__":  
+    model = load_model("Nets/0-1_tuned/net_1conv.h5")
+    im = cv2.imread("Datasets/Samples/0-6.png")
+    lv = Layer_Visualization(model, im)
+    lv.visualization()
